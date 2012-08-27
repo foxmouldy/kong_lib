5e5bf419ce71301c708dfef6619b7ad3a73dd4f1
Bradley Frank <frank.brad@gmail.com>
2012-08-27 17:00:07 +0200

    Introduced split_spec and brad_lib as helper
    ngc3109_pipelinesettings.txt = settings.txt
    Added brad_lib.py which just has get_chans()
    Added split_spec.py which is from ngc3109_pipelinev2.py

---
+++ split_spec.py
@@ -0,0 +1,46 @@
+import sys
+from tasks import *
+from taskinit import *
+import casac
+import pylab as pl
+import os
+from optparse import OptionParser 
+import brad_lib as br
+
+usage = "usage: %prog options"
+parser = OptionParser(usage=usage);
+
+# Input MS
+parser.add_option("--vis", type = 'string', dest = 'vis', default=None, 
+	help = 'Input visibility to be splitted [None]');
+
+# Output MS
+parser.add_option("--splitvis", type='string', dest='splitvis', default=None, 
+	help = 'Name of Splitted MS [None]'
+
+# Centre Frequency 
+parser.add_option('--fi', type = 'string', dest='fi', default=None, 
+	help = "Centre frequency about which to extract MS [1418MHz]")
+
+# Number of Channels
+parser.add_option('di', type='string', dest='di', default=None, 
+	help = 'Number of channels on either side of fi to subtract [100]')
+
+if len(sys.argv)==1: 
+	parser.print_help();
+	dummy = sys.exit(0);
+
+spw_lower, spw_upper = br.get_chans(options.vis, fi=float(options.fi), 
+	di = int(options.di))
+
+print '\n'
+print '\n'
+print 'Splitting '+msfile+' from '+spw_lower+' to '+spw_upper;
+print '\n'
+print '\n'
+
+tag = options.retag+'.'+spw_lower+'to'+spw_upper;
+split(vis = options.vis, 
+	outputvis = options.splitvis, 
+	datacolumn = 'data', 
+	spw = '0:'+spw_lower+'~'+spw_upper);
