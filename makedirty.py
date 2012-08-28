bb882eda4c29f083495d716d5a045c83d2dc2a93
Bradley Frank <frank.brad@gmail.com>
2012-08-28 20:00:59 +0200

    Added makedirty.py script
    Simply makes dirty images for your sources.

---
+++ makedirty.py
@@ -0,0 +1,46 @@
+import sys
+from tasks import *
+from taskinit import *
+import casac
+import pylab as pl
+import os
+from optparse import OptionParser 
+
+usage = "usage: %prog options"
+parser = OptionParser(usage=usage);
+
+# vis
+parser.add_option("--vis", type = 'string', dest = 'vis', default=None, 
+	help = 'Input MS to be imaged');
+
+# sources to be imaged
+parser.add_option("--sources", type='string', dest='sources', default=None, 
+	help = "Sources to be imaged src1,src2,src3....[None]")
+
+# output tag
+parser.add_option("--tag", type='string', dest='tag', default=None, 
+	help = "Tag for output images [vistag]");
+
+# cellsize 
+parser.add_option("--cell", type='string', dest='cell', default='30.0arcsec', 
+	help = "Cell size for image [30.0arcsec]");
+
+(options, args) = parser.parse_args();
+
+if len(sys.argv)==1: 
+	parser.print_help();
+	dummy = sys.exit(0);
+
+if options.tag!=None: 
+	tag = options.tag;
+else:
+	tag = vis;
+
+for source in options.sources.split(','):
+	print "\n"
+	print "Making dirty image for source"+source;
+	print "\n"
+	clean(vis=options.vis, imagename=tag+source+'.dirty', niter=0, 
+		cell = options.cell);
+
+
